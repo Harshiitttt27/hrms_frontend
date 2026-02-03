df03e7f5adb248d16142ee6fc9d0c0401cdda53e
harshit <harrysethi52@gmail.com>
2026-02-03 18:21:21 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://hrms-backend-6grq.onrender.com/api"
+    : "http://127.0.0.1:8000/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api",
+  baseURL,
+  withCredentials: true,
 });
 
 export default api;
